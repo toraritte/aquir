483d647106c1bdfec33b419bdef3cdf265f88705
Attila Gulyas <toraritte@gmail.com>
2018-09-04 22:23:57 -0700

    Configure commanded_ecto_projections

--- mix.exs
+++ mix.exs
@@ -56,7 +56,7 @@ defmodule Aquir.Mixfile do
       {:commanded_eventstore_adapter, "~> 0.3"},
       {:comeonin, "~> 4.1"},
       {:pbkdf2_elixir, "~> 0.12"},
-      {:commanded_ecto_projections, "~> 0.6"},
+      {:commanded_ecto_projections, "~> 0.7"},
     ]
   end
 
