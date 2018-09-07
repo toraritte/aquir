9d0e1d58d55449251213cab338eea9b1ffab22a6
Attila Gulyas <toraritte@gmail.com>
2018-09-07 14:45:04 -0700

    Add Accounts context via Conduit book

---
+++ lib/aquir_web/controllers/fallback_controller.ex
@@ -0,0 +1,20 @@
+defmodule AquirWeb.FallbackController do
+  @moduledoc """
+  Translates controller action results into valid `Plug.Conn` responses.
+
+  See `Phoenix.Controller.action_fallback/1` for more details.
+  """
+  use AquirWeb, :controller
+
+  def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
+    conn
+    |> put_status(:unprocessable_entity)
+    |> render(AquirWeb.ChangesetView, "error.json", changeset: changeset)
+  end
+
+  def call(conn, {:error, :not_found}) do
+    conn
+    |> put_status(:not_found)
+    |> render(AquirWeb.ErrorView, :"404")
+  end
+end
