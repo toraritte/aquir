c4dcf476cce90bdf7f42be06e6d07b7a277c5d60
Attila Gulyas <toraritte@gmail.com>
2018-09-07 15:27:56 -0700

    Add User aggregate to Accounts

---
+++ lib/aquir/accounts/aggregates/user.ex
@@ -0,0 +1,32 @@
+defmodule Aquir.Accounts.Aggregates.User do
+
+  defstruct [
+    :uuid,
+    :username,
+    :email,
+    :hashed_password
+  ]
+
+  alias __MODULE__
+
+  @doc """
+  Register a new user.
+  """
+  def execute(%User{uuid: nil}, %RegisterUser{} = r) do
+    %UserRegistered{
+      uuid:     r.uuid,
+      username: r.username,
+      email:    r.email,
+      hashed_password: r.hashed_password
+    }
+  end
+
+  def apply(%User{} = user, %UserRegistered{} = r) do
+    %User{ user |
+      uuid:     r.uuid,
+      username: r.username,
+      email:    r.email,
+      hashed_password: r.hashed_password
+    }
+  end
+end
