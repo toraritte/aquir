ab5dd6164d992b3b874b2c22a45482fe47656500
Attila Gulyas <toraritte@gmail.com>
2018-09-30 22:29:04 -0700

    Add Accounts username and email columns case insensitive

--- priv/repo/migrations/20180906041353_create_accounts_users.exs
+++ priv/repo/migrations/20180906041353_create_accounts_users.exs
@@ -2,10 +2,13 @@ defmodule Aquir.Repo.Migrations.CreateAccountsUsers do
   use Ecto.Migration
 
   def change do
+    # https://hexdocs.pm/ecto/Ecto.Changeset.html#unique_constraint/3-case-sensitivity
+    execute "CREATE EXTENSION IF NOT EXISTS citext"
+
     create table(:accounts_users, primary_key: false) do
       add :uuid, :uuid, primary_key: true
-      add :username, :string
-      add :email, :string
+      add :username, :citext
+      add :email,    :citext
       add :hashed_password, :string
 
       timestamps()
