a32e06919b8776a64a0685807033f7eba63287f2
Attila Gulyas <toraritte@gmail.com>
2018-10-11 08:54:54 -0700

    Fix Auth.validate_password (missing arg)

--- lib/aquir/accounts/auth.ex
+++ lib/aquir/accounts/auth.ex
@@ -3,5 +3,5 @@ defmodule Aquir.Accounts.Auth do
   alias Comeonin.Bcrypt
 
   def hash_password(password),     do: Bcrypt.hashpwsalt(password)
-  def validate_password(password), do: Bcrypt.checkpw(password, hash)
+  def validate_password(password, hash), do: Bcrypt.checkpw(password, hash)
 end
