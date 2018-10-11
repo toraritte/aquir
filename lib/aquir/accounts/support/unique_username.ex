44e6173ceb32c7a95f04d9c7cb3987babb02c05e
Attila Gulyas <toraritte@gmail.com>
2018-10-11 05:30:47 -0700

    Comment on cast/2 vs update/3 in UniqueUsername

--- lib/aquir/accounts/support/unique_username.ex
+++ lib/aquir/accounts/support/unique_username.ex
@@ -10,6 +10,11 @@ defmodule Aquir.Accounts.Support.UniqueUsername do
 
     case name_is_taken do
       false ->
+        # `cast/2` would probably be  enough because a process
+        # is sequential internally, but  it is prudent to make
+        # sure  and  wait  for  an `:ok`  to  make  sure  that
+        # username  is  saved. For  a  larger  service a  more
+        # robust key-value store would be a better option.
         Agent.update(__MODULE__, &MapSet.put(&1, name))
         {:ok, :username_claimed}
       true  ->
