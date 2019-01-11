02079914d917ab3942b263093d2190779af53d99
Attila Gulyas <toraritte@gmail.com>
2019-01-11 08:34:06 -0800

    Add "fat" projector proposal

--- lib/aquir/accounts/projectors/user.ex
+++ lib/aquir/accounts/projectors/user.ex
@@ -4,10 +4,84 @@ defmodule Aquir.Accounts.Projectors.User do
     repo: Aquir.Repo,
     consistency: :strong
 
-  # NOTE 2018-10-23_2154
+  # 2018-10-23_2154 QUESTION
   @doc """
   Where is `:consistency` above defined? Commanded.Projections.Ecto
   has  one file basically (ecto.ex) and it's not in there.
+  """
+
+  # 2019-01-11_0757 NOTE (Projectors.User -> Accounts.Projector)
+  @doc """
+  Premise
+  =======
+
+  Haven't  found  examples for  defining  associations
+  among  projections  in  _Building Conduit_,  only  a
+  warning  and  an obscure  (at  least  it feels  like
+  that  right now) example on page  106:
+
+  + the warning:
+
+    Projectors work  independently therefore  the tables
+    owned by  a projector  shouldn't query the  table of
+    another as they  may not be updated  yet, BUT events
+    in  a   projector  are  handled  in   order.
+
+    Quote: "**individual projectors must be  responsible
+    for populating all data they require.**"
+
+  + the example:
+
+  It  describes   how  to  reference  an   author  for
+  a   published   article.   First,   it   takes   the
+  `AuthorCreated` event, saves into its "blog_authors"
+  table  saving the  `:user_uuid` that  references the
+  user in the Accounts context.
+
+  That is, the relationship between
+  Accounts.(Aggregates.)User and Blog.(Aggregates.)Author
+  is either  **one-to-one** or  **one-to-many**. Right
+  now I don't  see a way to know it  for sure but this
+  fluidity may be an upside
+
+  It is hard to see the relationships though and
+
+    ISN'T THE WHOLE POINT FOR PROJECTIONS THAT
+            THEY CAN TAKE ANY FORM?
+
+  If  one wants  do  design a  specific database  with
+  them,  they  should be  able  to.  Or if  one  wants
+  a   graphic  database   with  completely   different
+  semantics, so be it. Or project selected events into
+  a drawing. Whatever.
+
+  QUESTIONs raised:
+  + 2019-01-11_0819
+
+  PROPOSAL
+  ========
+
+  The gist of the PREMISE is that in "Building Conduit"
+
+  + couldn't  find  any   associations  or  migrations
+    suggesting relationships between tables and
+
+  + every projector corresponds  to an aggregate
+    (`User` aggregate `User` projector etc.)
+
+  What if the projector handles wider range of events,
+  for example for an entire context? Or for an entire
+  app?
+
+  Possible corollary: "fat" projectors
+  """
+
+  #  2019-01-11_0819 QUESTION
+  @doc """
+  QUESTION for 2018-10-23_2154:
+  Why not query the  state of the aggregate processes?
+  The same  lag applies to  them so they may  not hold
+  the most up to date information?
   """
 
   alias Aquir.Accounts.{
