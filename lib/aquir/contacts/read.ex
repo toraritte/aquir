3d46d99fc0fbf8af24933200d6c3916f20f51656
Attila Gulyas <toraritte@gmail.com>
2019-02-06 14:56:32 -0800

    (!) Add Contacts Read and Write

--- lib/aquir/contacts/read.ex
+++ lib/aquir/contacts/read.ex
@@ -1,2 +1,16 @@
 defmodule Aquir.Contacts.Read do
+
+  import Ecto.Query
+
+  alias Aquir.Repo
+  alias Aquir.Commanded.Read, as: ACRead
+  alias __MODULE__.Schemas, as: RS
+
+  def get_email_by([email_id: _] = keyword) do
+    ACRead.get_by(RS.Email, keyword)
+  end
+
+  def get_contact_by([contact_id: _] = keyword) do
+    ACRead.get_by(RS.Contact, keyword)
+  end
 end
--- lib/aquir/contacts/write.ex
+++ lib/aquir/contacts/write.ex
@@ -1,19 +1,69 @@
 defmodule Aquir.Contacts.Write do
 
-  def add_new_contact(
+  alias Aquir.Commanded.Support, as: ACS
+  # alias Aquir.Commanded.Router,  as: ACR
+
+  alias Aquir.Contacts.{
+    Read,
+    Commands,
+  }
+  alias Read.Schemas, as: RS
+
+  def add_contact(
     %{
       "first_name"  => fname,
       "middle_name" => mname,
       "last_name"   => lname,
     }
   ) do
+
+    [contact_id] = ACS.generate_uuids(1)
+
+    imbue_tuples = [{
+      %Commands.AddContact{},
+      %{
+        contact_id:  contact_id,
+        first_name:  fname,
+        middle_name: mname,
+        last_name:   lname,
+      }
+    }]
+
+    ACS.no_claim_and_dispatch(
+      imbue_tuples,
+      # TODO preload all keys that can be preloaded!
+      fn() -> Read.get_contact_by(contact_id: contact_id) end,
+      consistency: :strong)
   end
 
   def add_email(
+    %RS.Contact{} = contact,
     %{
-      "email" => email
+      "email" => email,
+      "type"  => type,
     }
   ) do
+
+    [email_id] = ACS.generate_uuids(1)
+
+    imbue_tuples = [{
+      %Commands.AddEmail{},
+      %{
+        email_id:   email_id,
+        contact_id: contact.contact_id,
+        email:      email,
+        type:       type,
+      }
+    }]
+
+    claims = [email: email]
+
+    ACS.claim_and_dispatch(
+      imbue_tuples,
+      claims,
+      fn() -> Read.get_email_by(email_id: email_id) end,
+      consistency: :strong
+    )
   end
 
   # 2019-02-04_1304 TODO Look up county by address/zip
@@ -26,7 +76,7 @@ defmodule Aquir.Contacts.Write do
       "county"         => county,
       "state"          => state,
       "ZIP"            => zip,
-      "category"       => category, # private or community residential, or commercial
+      "category"       => category, #  commercial, private or community residential
       "type"           => type, # private: rent, own, etc, community: assisted or independent living, nursing home etc.
     }
   ) do
@@ -34,14 +84,14 @@ defmodule Aquir.Contacts.Write do
 
   def add_work_address(
     %{
-      "street_address"  => street_address,
-      "unit"            => unit,
-      "city"            => city,
-      "county"          => county,
-      "state"           => state,
-      "ZIP"             => zip,
-      "category"        => "commercial",
-      "type"            => "none", # what else?
+      "street_address" => street_address,
+      "unit"           => unit,
+      "city"           => city,
+      "county"         => county,
+      "state"          => state,
+      "ZIP"            => zip,
+      "category"       => "commercial",
+      "type"           => "none", # what else?
     }
   ) do
   end
